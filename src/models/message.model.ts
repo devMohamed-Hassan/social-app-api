422e4af02de337cc7b59be02799dd942039b32ec
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2025-11-06 00:30:04 +0200

    Add Document type to IMessage interface

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -1,4 +1,4 @@
-import { model, Schema, Types } from "mongoose";
+import { Document, model, Schema, Types } from "mongoose";
 
 export interface IMessage extends Document {
   conversation: Types.ObjectId;
